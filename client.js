17783f9121ddce618405c58f2d0c0efb63bc3c04
VonderVuflya <97serdj97@gmail.com>
2020-05-02 12:32:42 +0300

    update locale for server tests

--- client.js
+++ client.js
@@ -20,7 +20,7 @@ const getFeedbackByProductViewData = async (product, actualize = false) => {
         const user = uesrsRes.data.users.find((user) => user.id === comment.userId);
         return {
           ...comment,
-          date: new Date(comment.date).toLocaleString().split(" ")[0],
+          date: new Date(comment.date).toLocaleString('ru').split(" ")[0],
           user: `${user.name} (${user.email})`,
         };
       })
