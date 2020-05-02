ee70cd61fd21e5125c5176ad7b502b76dd462276
VonderVuflya <97serdj97@gmail.com>
2020-05-02 11:14:03 +0300

    done 1 & so so done 2...

--- client.js
+++ client.js
@@ -1,7 +1,30 @@
 const { axios } = require("./fakeBackend/mock");
 
 const getFeedbackByProductViewData = async (product, actualize = false) => {
+  const feedbackRes = await axios.get(`/feedback`, {
+    params: {
+      product,
+    },
+  });
 
+  const userIds = feedbackRes.data.feedback.map((comment) => comment.userId);
+  const uesrsRes = await axios.get(`/users`, {
+    params: {
+      ids: userIds,
+    },
+  });
+
+  const feedback = feedbackRes.data.feedback.map((comment) => {
+    const user = uesrsRes.data.users.find((user) => user.id === comment.userId);
+    return {
+      ...comment,
+      user,
+    };
+  });
+
+  return {
+    feedback: [...feedback],
+  };
 };
 
 module.exports = { getFeedbackByProductViewData };
